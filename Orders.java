37bde4f1239eb3974896b1fe2e57057ada58e6d6
hyo kyung kang <hyokyung-kang@uiowa.edu>
2016-11-18 16:44:47 -0500

    Update Orders.java

--- Orders.java
+++ Orders.java
@@ -1,9 +1,10 @@
 package production;
 /**
-*
-*@author prof Ted Herman
-*
-/
+ * 
+ * @author prof Ted Herman
+ *
+ */
+
 public interface Orders {
   Order getRandomOrder();  // null if no Order can be made, else an Order
   }
