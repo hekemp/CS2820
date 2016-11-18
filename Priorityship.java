5aeb3716e07dae137b57d770387acbe50fc048d2
hyo kyung kang <hyokyung-kang@uiowa.edu>
2016-11-18 16:38:40 -0500

    Update Priorityship.java

--- Priorityship.java
+++ Priorityship.java
@@ -4,7 +4,7 @@ package production;
  * @author hyo kyung kang
  *
  */
-public class Priorityship extends Orders{
+public class Priorityship extends Order{
 
 	public Priorityship(String progress, String address, String dateReceived, int orderNumber) {
 		super(progress, address, dateReceived, orderNumber);
