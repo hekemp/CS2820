2a09620b58bf11e3a437328cc3b7b6e8d9f66521
yunfjiang <yunfan-jiang@uiowa.edu>
2016-12-05 17:14:20 -0600

    Update Shelf.java

--- Shelf.java
+++ Shelf.java
@@ -9,16 +9,27 @@ class Shelf{
 	int items = 0;
 	ArrayList<item> Item = new ArrayList<>();
 	int x, y;
+	/**@author yunfjiang
+	 * Added a Point/location 
+	 * and a boolean to tell if full
+	 */
+	Point location; 
+	boolean iffull=false;
+	
 	public Shelf(Point P){
 		this.x = P.x;
 		this.y = P.y;
 		P.shelf = true;
+		this.location =P;
 	}
 	
 	public String addItems(item I){
 		Item.add(I);
 		items++;
-		if(items == 20) return "This shelf is full.";
+		if(items == 20) {
+			iffull=true;
+			return "This shelf is full.";
+		}
 		if(items > 20){
 			items--;
 			return "Load fails, shelf is full.";
@@ -30,6 +41,23 @@ class Shelf{
 		Item.remove(Item.indexOf(I));
 		items--;
 		return String.format("Removed item %1$d %s from shelf", I.itemID, I.type);
+	}
+	
+	/**@author yunfjiang
+	 * added three getters
+	 * to get location and coordinators
+	 */
+	public Point getlocation(){
+		return location;
+	}
+	public int getX(){
+		return this.x;
+	}
+	public int getY(){
+		return this.y;
+	}
+	public boolean full(){
+		return iffull;
 	}
 	
 	// floor will memorize the new location where robot
