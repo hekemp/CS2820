c1c2b676350edca39057ba313533c4dbdb1ea9cd
hekemp <heather-kemp@uiowa.edu>
2016-12-06 18:26:52 -0600

    Create MockOrder.java

---
+++ MockOrder.java
@@ -0,0 +1,11 @@
+package production;
+
+import java.util.*;
+
+/**
+ * 
+ * @author Heather Kemp
+ * The MockOrder object will function as a very fundamental test case containing one order and one item.
+ * It manages instances of the Belt and Robot in order to execute calls to these methods.
+ *
+ */
