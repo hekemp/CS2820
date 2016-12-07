f037d89f68107a4ed2314f24d8496f42b78c906d
Qiansan <xinyu-qian@uiowa.edu>
2016-12-06 21:48:36 -0600

    Update robotPath.java

--- robotPath.java
+++ robotPath.java
@@ -1,6 +1,11 @@
 package production;
+/**
+ *
+ * @author Xinyu Qian
+ */
+import java.util.ArrayList;
 // interface for robot to use 
 // in order to  return a path from floor
 public interface robotPath {
-	public Point[] getRoute(Point p1, Point p2);
+	ArrayList<Point> getRoute(Point p1, Point p2);
 }
