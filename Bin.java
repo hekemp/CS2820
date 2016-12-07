8fa74e5078e36c910eeea475debce5543d46431d
melkeurti <mounaikram-elkeurti@uiowa.edu>
2016-12-07 14:46:57 -0600

    Update Bin.java

--- Bin.java
+++ Bin.java
@@ -13,11 +13,6 @@ public class Bin {
   Order order; 
   
   public Bin() {}
-	  
-  public Bin getBin(){
-	  Bin bin = new Bin();
-	  return bin;
-  }
   
   public String toString() { return "Bin"; }
 }
