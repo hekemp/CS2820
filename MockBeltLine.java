d38ab62b5dfbb5ea9320316f4ed90289c1be0624
hekemp <heather-kemp@uiowa.edu>
2016-12-08 09:36:04 -0600

    Update MockBeltLine.java

--- MockBeltLine.java
+++ MockBeltLine.java
@@ -7,64 +7,62 @@
  *
  */
 
+package production;
 
 import java.util.*;
 
-
 public class MockBeltLine implements Event{
-	
+    
    
    int myX;
    int myY;
    String myItem;
    private ArrayList<Event> myEvents;
   private ArrayList<String> myParameters;
-	
-	public MockBeltLine(int x, int y, String item){
+    
+    public MockBeltLine(int x, int y, String item){
     myX = x;
     myY = y;
     myItem = item;
     myEvents = new ArrayList<Event>();
     myParameters = new ArrayList<String>();
-	}
-	
-	/**
-	 * Performs an action. Extracts the method to be executed and parameters from the method strong and then executes these
-	 * methods.
-	 * @input - a string which contains a method name/key and parameters, seperated by commas
-	 */
-	public void performAction(String Method){
-	    if (myX == 0 && myY == 0)
-	    	{}
-	    else{
-		if(myX != 0)
-			{int tempX = myX;
-			myX = myX - 1;
-			System.out.println(myItem + " moved from: " + String.valueOf(tempX) + " to " + String.valueOf(myX));
-		        myEvents.add((Event) this);
-		        myParameters.add(" ");}
-		else
-			{int tempY = myY;
-			myY = myY - 1;
-			System.out.println(myItem + " moved from: " + String.valueOf(tempY) + " to " + String.valueOf(myY));
-			myEvents.add((Event) this);
-			myParameters.add(" ");}
-	    }
-	}
-	
-	/**
-	 * Gets the event. Returns this object as an Event
-	 */
-	
-	public Event getEvent(){
+    }
+    
+    /**
+     * Performs an action. Extracts the method to be executed and parameters from the method strong and then executes these
+     * methods.
+     * @input - a string which contains a method name/key and parameters, seperated by commas
+     */
+    public void performAction(String Method){
+        while(!(myX == 0 && myY == 0))
+        { 
+            if(myX > 0)
+            {int tempX = myX;
+            myX = myX - 1;
+            System.out.println(myItem + " moved from: X = " + String.valueOf(tempX) + " to X = " + String.valueOf(myX));}
+        else if (myY > 0)
+            {int tempY = myY;
+            myY = myY - 1;
+            System.out.println(myItem + " moved from: Y = " + String.valueOf(tempY) + " to Y = " + String.valueOf(myY));}
+        else;
+        }
+        
+    }
+    
+    /**
+     * Gets the event. Returns this object as an Event
+     */
+    
+    public Event getEvent(){
 
-	     return (Event) this;
-	   }
-	/**
-	 * Gets the parameters. Returns the parameter for the next call of this method.
-	 */
-	
-	public String getPara(){
-	    return "Null";
-	   }
+         return (Event) this;
+       }
+    /**
+     * Gets the parameters. Returns the parameter for the next call of this method.
+     */
+    
+    public String getPara(){
+        if(myX == 0 && myY == 0) return " ";
+        else return "Continuing";
+       }
 }
