a37a3b6009d7f3445a41f7637abb47c01025ff33
Xinglzhao <xingluzhao2015@gmail.com>
2016-12-08 14:13:10 -0600

    Add files via upload

---
+++ MockRobot.java
@@ -0,0 +1,52 @@
+
+import java.util.ArrayList;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zhaoxinglu
+ * This class make a fake robot object
+ */
+public class MockRobot {
+    Floor m;
+    Point robot=new Point(6,0);
+    ArrayList<Point> robotroute=new ArrayList<Point>();
+   
+    /**
+ * @author zhaoxinglu
+ * @param Point, indicate the coordinate of robot in the map
+ * 
+ */
+    public MockRobot(Floor m1){
+        
+        m=m1;
+        
+       
+        
+       
+        
+    }//initialize where robot is originally and continuously move
+
+   public void move(Point P){
+       robot=P;
+   }
+    public Point TrackRobot(int n){
+        robotroute=m.getRoute(m.getCharger(),m.getPicker());
+        move(robotroute.get(n));
+        
+       return robot;
+   }
+   /**
+ * @author zhaoxinglu
+ * @return Point, keep track of where robot is in the map
+ */
+   
+    
+}
