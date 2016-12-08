cf2ac0b2ee63305cb6cdd02ec8a7a36eb71c1792
hekemp <heather-kemp@uiowa.edu>
2016-12-07 20:29:55 -0600

    Update Production.java

--- Production.java
+++ Production.java
@@ -12,23 +12,23 @@ public class Production
 {
     public static void main(String[] args){
         Floor newFloor = new Floor();
-        Belt newBelt = new Belt(newFloor);
+        //Belt newBelt = new Belt(newFloor);
         inventory newInventory = new inventory();
-        Order newOrder = new Order();
+       // Order newOrder = new Order();
         // Please do not edit code that will not compile into a piece that will compile otherwise
         //Order newOrder = new Order(String asdf ,String asxcv ,String zxcvb, int p, OrderItem[] orderutemsdfg);
         Shelf newShelf = new Shelf();
         Robot newRobot = new Robot(newFloor,newShelf,2,2,100);
-        Visualizer newVisualizer = new Visualizer();
+        //Visualizer newVisualizer = new Visualizer();
         
         Master myMaster = new Master();
-        myMaster.addBelt(newBelt);
+        //myMaster.addBelt(newBelt);
         myMaster.addFloor(newFloor);
         myMaster.addInventory(newInventory);
-        myMaster.addOrder(newOrder);
+        //myMaster.addOrder(newOrder);
         myMaster.addRobot(newRobot);
         myMaster.addShelf(newShelf);
-        myMaster.addVisualizer(newVisualizer);
+        //myMaster.addVisualizer(newVisualizer);
         myMaster.runSimulation();
     }
 }
